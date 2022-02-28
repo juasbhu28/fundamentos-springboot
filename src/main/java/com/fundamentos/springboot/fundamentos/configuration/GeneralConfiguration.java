9412bf8b4408f1941bef63e1b21bc4c59248b5a3
Juan Sebastian Botello Hurtado <juasbhu@hotmail.com>
2022-02-28 03:07:03 -0500

    Adding sourcer from file propierties and @Bean - DI

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentos.springboot.fundamentos.bean.impl.MyBeanWithPropiertiesImp
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -25,6 +28,16 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithPropierties myBeanWithPropiertiesFuncion(){
         return new MyBeanWithPropiertiesImpl(name, apellido);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.driverClassName("org.h2.Driver");
+        builder.url("jdbc:h2:mem:testdb");
+        builder.username("SA");
+        builder.password("");
+        return builder.build();       
     }
 
 }
